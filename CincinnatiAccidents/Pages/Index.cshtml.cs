17340c290bceef22cc346024085cb7bd23e93df6
praneeth465 <praneeth465@gmail.com>
2019-11-10 14:18:08 -0500

    Group Project - initial commit

---
+++ CincinnatiAccidents/Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using QuickTypeFire;
+using QuickTypeTraffic;
+
+namespace CincinnatiAccidents.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        { 
+            using (var webClient = new WebClient())
+            {
+                String trafficjsonString = webClient.DownloadString("https://data.cincinnati-oh.gov/resource/rvmt-pkmq.json");
+                var trafficAccidents = TrafficAccident.FromJson(trafficjsonString);
+                ViewData["trafficAccidents"] = trafficAccidents;
+
+                String firejsonString = webClient.DownloadString("https://data.cincinnati-oh.gov/resource/vnsz-a3wp.json");
+                var fireAccidents = FireAccident.FromJson(firejsonString);
+                ViewData["fireAccidents"] = fireAccidents;
+            }
+        }
+    }
+}
